fe804a7d7edb61982025db53e9903fde3ae52504
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-05 17:09:36 +0000

    [Finish]2-10

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -26,7 +26,7 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
